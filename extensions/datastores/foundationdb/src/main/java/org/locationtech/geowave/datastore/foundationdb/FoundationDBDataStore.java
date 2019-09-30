5592b455f004b3768fa23fe91e72a104cf8ebe98
Aniroodh Ravikumar <ar765@cornell.edu>
2019-09-30 14:08:24 -0400

    Foundation db writer (#8)
    * Add foundationdb datastore module
    
    * Add FDB dependency
    
    * Do we need this?
    
    * Add fdb version to the parent POM
    
    * Add base package
    
    * Add skeleton code for core fdb classes
    
    * Don't need these todos
    
    * Add FDB interaction interface
    
    * Add FDB write class
    
    * fix eclipse "Source folder is not a Java project"
    
    with `mvn eclipse:eclipse`
    
    * We don't actually need this interface
    
    * Add FDB metadata table and writer with stubs
    
    * Fix formatting
    
    * made stubs for writer
    
    * made stubs for writer
    
    * implement FDB metatable add
    
    * Add skeleton code for fdb metadata reader class
    
    * stubs for reader
    
    * Wrote the different FBDReader constructors, and wrote stubs for the various createIterator methods
    
    * Add FoundationDBMetadataIterator class with stubbed method implementation
    
    * Add fdb metadata class (basically identical to the rocks db metadata class)
    
    * Added remaining methods that weren't necessary for type-checking within FDBReader
    
    * I think we can reuse the RocksDB code for readRow, since we did that for Table.add
    
    * implement readRow
    
    * implement FoundationDBRow
    
    * created config provider and client cache for FoundationDB
    
    * use FoundationDBRequiredOptions
    
    * Recompiled with maven to potentially fix formatting issues
    
    * delete ConfigProvider class

--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/FoundationDBDataStore.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/FoundationDBDataStore.java
@@ -10,6 +10,7 @@ package org.locationtech.geowave.datastore.foundationdb;
 
 import org.locationtech.geowave.core.store.DataStoreOptions;
 import org.locationtech.geowave.core.store.metadata.*;
+import org.locationtech.geowave.datastore.foundationdb.operations.FoundationDBOperations;
 import org.locationtech.geowave.mapreduce.BaseMapReduceDataStore;
 import org.locationtech.geowave.mapreduce.MapReduceDataStoreOperations;
 import java.io.Closeable;
@@ -32,6 +33,6 @@ public class FoundationDBDataStore extends BaseMapReduceDataStore implements Clo
 
   @Override
   public void close() throws IOException {
-    // TODO: implement FoundationDBOperations
+    ((FoundationDBOperations) baseOperations).close();
   }
 }
