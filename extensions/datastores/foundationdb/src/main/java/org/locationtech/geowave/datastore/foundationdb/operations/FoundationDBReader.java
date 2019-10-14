893fca4b72ae1eba4bb016c82af4aa5aca38b131
aniroodh-ravikumar <ar765@cornell.edu>
2019-10-13 21:03:31 -0400

    Removed unwanted import statements

--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBReader.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBReader.java
@@ -1,20 +1,10 @@
 package org.locationtech.geowave.datastore.foundationdb.operations;
 
 import java.io.Closeable;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.tuple.Pair;
-import org.locationtech.geowave.core.index.ByteArrayRange;
 import org.locationtech.geowave.core.index.SinglePartitionQueryRanges;
 import org.locationtech.geowave.core.store.CloseableIterator;
 import org.locationtech.geowave.core.store.CloseableIteratorWrapper;
@@ -28,12 +18,7 @@ import org.locationtech.geowave.core.store.operations.RowReader;
 import org.locationtech.geowave.core.store.query.filter.ClientVisibilityFilter;
 import org.locationtech.geowave.core.store.util.DataStoreUtils;
 import org.locationtech.geowave.datastore.foundationdb.util.FoundationDBClient;
-import org.locationtech.geowave.datastore.foundationdb.util.FoundationDBDataIndexTable;
-// import org.locationtech.geowave.datastore.foundationdb.util.RocksDBUtils;
-import org.locationtech.geowave.mapreduce.splits.GeoWaveRowRange;
 import org.locationtech.geowave.mapreduce.splits.RecordReaderParams;
-import com.google.common.collect.Iterators;
-import com.google.common.collect.Sets;
 import com.google.common.collect.Streams;
 
 public class FoundationDBReader<T> implements RowReader<T> {
