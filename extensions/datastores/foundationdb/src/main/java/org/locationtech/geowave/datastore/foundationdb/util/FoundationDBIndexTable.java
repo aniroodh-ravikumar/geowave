eab50c3369b04f196136232e26837aa253d0ad8e
Kevin Chan <kevinchan159@gmail.com>
2019-10-24 20:05:26 -0400

    Implement delete for FoundationDBIndexTable

--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/util/FoundationDBIndexTable.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/util/FoundationDBIndexTable.java
@@ -1,14 +1,21 @@
 package org.locationtech.geowave.datastore.foundationdb.util;
 
+import com.apple.foundationdb.Database;
 import com.apple.foundationdb.async.AsyncIterable;
 import com.apple.foundationdb.async.AsyncIterator;
-import com.apple.foundationdb.Database;
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Longs;
 import org.locationtech.geowave.core.index.ByteArrayRange;
+import org.locationtech.geowave.core.index.ByteArrayRange;
+import org.locationtech.geowave.core.index.ByteArrayUtils;
 import org.locationtech.geowave.core.store.CloseableIterator;
 import org.locationtech.geowave.core.store.entities.GeoWaveRow;
 import org.locationtech.geowave.core.store.entities.GeoWaveValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FoundationDBIndexTable extends AbstractFoundationDBTable {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FoundationDBIndexTable.class);
   private final boolean requiresTimestamp;
   private final byte[] partition;
 
@@ -22,7 +29,19 @@ public class FoundationDBIndexTable extends AbstractFoundationDBTable {
     this.requiresTimestamp = requiresTimestamp;
   }
 
-  public void delete(final byte[] sortKey, final byte[] dataId) {}
+  public void delete(final byte[] sortKey, final byte[] dataId) {
+    final Database db = getDb();
+    try {
+      final byte[] prefix = Bytes.concat(sortKey, dataId);
+      final byte[] nextPrefix = ByteArrayUtils.getNextPrefix(prefix);
+      db.run(tr -> {
+        tr.clear(prefix, nextPrefix);
+        return null;
+      });
+    } catch (final Exception e) {
+      LOGGER.warn("Unable to delete by sort key and data ID", e);
+    }
+  }
 
   public synchronized void add(
       final byte[] sortKey,
