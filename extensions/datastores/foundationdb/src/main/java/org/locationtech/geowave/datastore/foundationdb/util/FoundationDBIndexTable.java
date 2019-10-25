4fdba9279e5d432c5b9df0ff761acaa5f1eb10fb
Kevin Chan <kevinchan159@gmail.com>
2019-10-24 20:58:59 -0400

    Finish basic implementation of AbstractFoundationDBIndexTable & FoundationDBIndexTable

--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/util/FoundationDBIndexTable.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/util/FoundationDBIndexTable.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 public class FoundationDBIndexTable extends AbstractFoundationDBTable {
   private static final Logger LOGGER = LoggerFactory.getLogger(FoundationDBIndexTable.class);
+  private long prevTime = Long.MAX_VALUE;
   private final boolean requiresTimestamp;
   private final byte[] partition;
 
@@ -49,31 +50,68 @@ public class FoundationDBIndexTable extends AbstractFoundationDBTable {
       final byte[] sortKey,
       final byte[] dataId,
       final short numDuplicates,
-      final GeoWaveValue value) {}
+      final GeoWaveValue value) {
+    byte[] key;
+    byte[] endBytes;
+    if (visibilityEnabled) {
+      endBytes =
+          Bytes.concat(
+              value.getVisibility(),
+              ByteArrayUtils.shortToByteArray(numDuplicates),
+              new byte[] {
+                  (byte) value.getVisibility().length,
+                  (byte) sortKey.length,
+                  (byte) value.getFieldMask().length});
+    } else {
+      endBytes =
+          Bytes.concat(
+              ByteArrayUtils.shortToByteArray(numDuplicates),
+              new byte[] {(byte) sortKey.length, (byte) value.getFieldMask().length});
+    }
+    if (requiresTimestamp) {
+      // sometimes rows can be written so quickly that they are the exact
+      // same millisecond - while Java does offer nanosecond precision,
+      // support is OS-dependent. Instead this check is done to ensure
+      // subsequent millis are written at least within this ingest
+      // process.
+      long time = Long.MAX_VALUE - System.currentTimeMillis();
+      if (time >= prevTime) {
+        time = prevTime - 1;
+      }
+      prevTime = time;
+      key = Bytes.concat(sortKey, dataId, Longs.toByteArray(time), value.getFieldMask(), endBytes);
+    } else {
+      key = Bytes.concat(sortKey, dataId, value.getFieldMask(), endBytes);
+    }
+    put(key, value.getValue());
+  }
 
   public CloseableIterator<GeoWaveRow> iterator() {
+    Database db = getDb();
+    if (db == null) {
+      return new CloseableIterator.Empty<>();
+    }
+    // TODO: Figure out what range should be.
+    // I think we should the range should begin at the beginning of the DB.
+    // return iterator(range)
     return null;
   }
 
   public CloseableIterator<GeoWaveRow> iterator(final ByteArrayRange range) {
-    try {
-      Database db = getDb();
-      if (db == null) {
-        return new CloseableIterator.Empty<>();
-      }
-      AsyncIterable iterable = db.run(tr -> {
-        return tr.getRange(range.getStart(), range.getEnd());
-      });
-      AsyncIterator iterator = iterable.iterator();
-      return new FoundationDBRowIterator(
-          iterator,
-          adapterId,
-          partition,
-          requiresTimestamp,
-          visibilityEnabled);
-    } catch (Exception e) {
+    Database db = getDb();
+    if (db == null) {
       return new CloseableIterator.Empty<>();
     }
+    AsyncIterable iterable = db.run(tr -> {
+      return tr.getRange(range.getStart(), range.getEnd());
+    });
+    AsyncIterator iterator = iterable.iterator();
+    return new FoundationDBRowIterator(
+        iterator,
+        adapterId,
+        partition,
+        requiresTimestamp,
+        visibilityEnabled);
   }
 
 }
