b1c45251d890dd5f524e9a51e0f20bbf114f0d66
Kirill Chernyshov <kc875@cornell.edu>
2019-12-08 21:09:40 -0500

    Add docstrings + class overview to FDBMReader

--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBMetadataReader.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBMetadataReader.java
@@ -16,11 +16,23 @@ import java.util.Iterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This class provides an abstraction for reading metadata.
+ */
 public class FoundationDBMetadataReader implements MetadataReader {
   private static final Logger LOGGER = LoggerFactory.getLogger(FoundationDBMetadataReader.class);
   private final FoundationDBMetadataTable table;
   private final MetadataType metadataType;
 
+  /**
+   * Create a reader for a given metadata type using a FDB Metadata table.
+   *
+   * Preconditions:
+   *  - The table is not closed
+   *
+   * @param table The table.
+   * @param metadataType The type of the metadata to read.
+   */
   public FoundationDBMetadataReader(
       final FoundationDBMetadataTable table,
       final MetadataType metadataType) {
@@ -28,6 +40,13 @@ public class FoundationDBMetadataReader implements MetadataReader {
     this.metadataType = metadataType;
   }
 
+  /**
+   * Read metadata, as specified by the query.
+   *
+   * @param query The query.
+   * @param mergeStats TODO what does this do?
+   * @return An iterator that lazily loads the metadata as they are requested.
+   */
   public CloseableIterator<GeoWaveMetadata> query(
       final MetadataQuery query,
       final boolean mergeStats) {
@@ -67,6 +86,12 @@ public class FoundationDBMetadataReader implements MetadataReader {
     return isStats ? new StatisticsRowIterator(retVal, query.getAuthorizations()) : retVal;
   }
 
+  /**
+   * Read metadata, as specified by the query.
+   *
+   * @param query The query.
+   * @return An iterator that lazily loads the metadata as they are requested.
+   */
   @Override
   public CloseableIterator<GeoWaveMetadata> query(MetadataQuery query) {
     return this.query(query, true);
