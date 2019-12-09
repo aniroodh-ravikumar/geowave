111d29f981e5ec61a46351ec152b6830f40d41b5
Kirill Chernyshov <kc875@cornell.edu>
2019-12-08 20:57:48 -0500

    Add docstrings for FDBMWriter

--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBMetadataWriter.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBMetadataWriter.java
@@ -8,10 +8,26 @@ public class FoundationDBMetadataWriter implements MetadataWriter {
   private final FoundationDBMetadataTable table;
   private boolean closed = false;
 
+  /**
+   * Create a writer using a FDB Metadata table.
+   *
+   * Preconditions:
+   *  - The table is not closed
+   *
+   * @param table The table.
+   */
   public FoundationDBMetadataWriter(FoundationDBMetadataTable table) {
     this.table = table;
   }
 
+  /**
+   * Write metadata to the table.
+   *
+   * Preconditions:
+   *  - The writer is not closed
+   *
+   * @param metadata The metadata.
+   */
   @Override
   public void write(GeoWaveMetadata metadata) {
     if (table != null) {
@@ -19,6 +35,14 @@ public class FoundationDBMetadataWriter implements MetadataWriter {
     }
   }
 
+  /**
+   * Flush the writer, committing all pending writes. Note that the writes may
+   * already be committed - this method just establishes that they *must* be
+   * committed after the method returns.
+   *
+   * Preconditions:
+   *  - The writer is not closed
+   */
   @Override
   public void flush() {
     if (table != null) {
@@ -26,6 +50,11 @@ public class FoundationDBMetadataWriter implements MetadataWriter {
     }
   }
 
+  /**
+   * Close the writer, preventing any further writes. After calling this method,
+   * flush and write may no longer be called. After the writer is closed, there
+   * is no way to re-open it.
+   */
   @Override
   public void close() {
     if (!closed) {
